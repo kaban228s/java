cb6d9f216d48c0435a118777f5a54ff7e6cd80ff
kaban228s <sasrawegwe123@gmail.com>
2024-03-18 15:21:18 +0300

    asf

--- Main.java
+++ Main.java
@@ -1,26 +1,45 @@
-
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+public class Mian {
     public static void main(String[] args) {
-        Random random = new Random();
-
-        int [][] arr = new int[3][3];
-
-        for (int i = 0; i < 3; i++ ) {
-            for (int j = 0; j < 3; j++ )  {
-                arr[i][j] = random.nextInt(9)+1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("введите размер матрицы: ");
+        int size = input.nextInt();
+        int[][] arr = new int[size][size];
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = rand.nextInt(1,10 - 1);
             }
         }
-        System.out.println(toString(arr));
-    }
 
-    static String toString(int[][] arr) {
-        return Arrays.deepToString(arr)
-                .replace("[[", "|")
-                .replace("],", "|")
-                .replace("]]", "|")
-                .replace(" [", "\n|");
+        System.out.println(Arrays.deepToString(arr)
+                .replace("[[","|")
+                .replace("]]","|")
+                .replace(" [","|")
+                .replace("],","|\n")
+        );
+
+        int diag = 1;
+        int top = 0;
+        int bot = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j) {
+                    diag *= arr[i][j];
+                } else if (i < j) {
+                    top += arr[i][j];
+                } else if (i > j) {
+                    bot -= arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Произведение диагонали: " + diag);
+        System.out.println("Сумма верха: " + top);
+        System.out.println("Разность низа: " + bot);
     }
 }
