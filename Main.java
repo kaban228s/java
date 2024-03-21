8db9b107721996d7bf05d81504a49db6d546b6f1
kaban228s <sasrawegwe123@gmail.com>
2024-03-21 21:45:51 +0300

    asf

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Mian {
+public class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("введите размер матрицы: ");
